f2c7416302c2fe345f7bbb7ef24f547ea7fa4461
Geza Kovacs <gkovacs@mit.edu>
2010-04-11 22:33:01 -0400

    moved Search into its own file

--- sceneparse/Main.cs
+++ sceneparse/Main.cs
@@ -25,74 +25,8 @@ using System.Drawing;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 
-namespace sceneparse {
-	
-
-
-
-	public delegate void NodeActionDelegate(IVisNode n);
-	
-	public interface ISearchAlgorithm {
-		C5.IntervalHeap<IVisNode> Agenda {get; set;}
-		Dictionary<int[,], IVisNode> Visited {get; set;}
-		NodeActionDelegate NodeAction {get; set;}
-		int Lifetime {get; set;}
-		void Add(IVisNode n);
-		void Add(IEnumerable<IVisNode> nl);
-		bool Next();
-		void Run();
-	}
-	
-	public abstract class BaseSearchAlgorithm : ISearchAlgorithm {
-		public C5.IntervalHeap<IVisNode> Agenda {get; set;}
-		public Dictionary<int[,], IVisNode> Visited {get; set;}
-		public NodeActionDelegate NodeAction {get; set;}
-		public int Lifetime {get; set;}
-		public void Add(IVisNode n) {
-			this.Agenda.Add(n);
-			this.Visited.Add(n.Data, n);
-		}
-		public void Add(IEnumerable<IVisNode> nl) {
-			foreach (var n in nl) {
-				Add(n);
-			}
-		}
-		public virtual bool Next() {return false;}
-		public void Run() {
-			while (this.Next()) {};
-		}
-	}
-	
-	public class SearchDijkstra : BaseSearchAlgorithm {
-		public SearchDijkstra(NodeActionDelegate nadel) {
-			Agenda = new C5.IntervalHeap<IVisNode>(new VisNodeComparer());
-			Visited = new Dictionary<int[,], IVisNode>(new MatrixEqualityComparerInt());
-			NodeAction = nadel;
-			Lifetime = int.MaxValue;
-		}
-		public override bool Next() {
-			if (Lifetime != int.MaxValue) {
-				if (--Lifetime <= 0) return false;
-			}
-			IVisNode cn = null;
-			while (cn == null || cn.Cost > cn.MaxCost) {
-				if (Agenda.IsEmpty) return false;
-				cn = Agenda.DeleteMin();
-			}
-			NodeAction(cn);
-			var nvals = cn.Next();
-			foreach (var x in nvals) {
-				if (Visited.ContainsKey(x.Data)) {
-					continue;
-				} else {
-					Visited.Add(x.Data, x);
-					Agenda.Add(x);
-				}
-			}
-			return true;
-		}
-	}
-
+namespace sceneparse
+{
 	public class MainClass {
 		
 		public static IVisNode DeSerializeFromFile(string fn, string typen) {
---
+++ sceneparse/Search.cs
@@ -0,0 +1,93 @@
+// 
+//  Search.cs
+//  
+//  Author:
+//       Geza Kovacs <gkovacs@mit.edu>
+// 
+//  Copyright (c) 2010 Geza Kovacs
+// 
+//  This library is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation; either version 2.1 of the
+//  License, or (at your option) any later version.
+// 
+//  This library is distributed in the hope that it will be useful, but
+//  WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//  Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+using System;
+using System.IO;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace sceneparse
+{
+	public delegate void NodeActionDelegate(IVisNode n);
+	
+	public interface ISearchAlgorithm {
+		C5.IntervalHeap<IVisNode> Agenda {get; set;}
+		Dictionary<int[,], IVisNode> Visited {get; set;}
+		NodeActionDelegate NodeAction {get; set;}
+		int Lifetime {get; set;}
+		void Add(IVisNode n);
+		void Add(IEnumerable<IVisNode> nl);
+		bool Next();
+		void Run();
+	}
+	
+	public abstract class BaseSearchAlgorithm : ISearchAlgorithm {
+		public C5.IntervalHeap<IVisNode> Agenda {get; set;}
+		public Dictionary<int[,], IVisNode> Visited {get; set;}
+		public NodeActionDelegate NodeAction {get; set;}
+		public int Lifetime {get; set;}
+		public void Add(IVisNode n) {
+			this.Agenda.Add(n);
+			this.Visited.Add(n.Data, n);
+		}
+		public void Add(IEnumerable<IVisNode> nl) {
+			foreach (var n in nl) {
+				Add(n);
+			}
+		}
+		public virtual bool Next() {return false;}
+		public void Run() {
+			while (this.Next()) {};
+		}
+	}
+	
+	public class SearchDijkstra : BaseSearchAlgorithm {
+		public SearchDijkstra(NodeActionDelegate nadel) {
+			Agenda = new C5.IntervalHeap<IVisNode>(new VisNodeComparer());
+			Visited = new Dictionary<int[,], IVisNode>(new MatrixEqualityComparerInt());
+			NodeAction = nadel;
+			Lifetime = int.MaxValue;
+		}
+		public override bool Next() {
+			if (Lifetime != int.MaxValue) {
+				if (--Lifetime <= 0) return false;
+			}
+			IVisNode cn = null;
+			while (cn == null || cn.Cost > cn.MaxCost) {
+				if (Agenda.IsEmpty) return false;
+				cn = Agenda.DeleteMin();
+			}
+			NodeAction(cn);
+			var nvals = cn.Next();
+			foreach (var x in nvals) {
+				if (Visited.ContainsKey(x.Data)) {
+					continue;
+				} else {
+					Visited.Add(x.Data, x);
+					Agenda.Add(x);
+				}
+			}
+			return true;
+		}
+	}
+}
